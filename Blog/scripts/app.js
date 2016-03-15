0585df99f68ff4f028771c25582b4fc42dc9c592
John <john6tain.tube@gmail.com>
2016-03-15 16:55:12 +0200

    register working

--- Blog/scripts/app.js
+++ Blog/scripts/app.js
@@ -1,21 +1,30 @@
 var app = app || {};
 
-(function() {
+(function () {
     app.router = Sammy(function () {
-        this.get('#/', function() {
+        this.get('#/', function () {
             showHomeMenu(selector);
         });
 
-        this.get('#/login', function() {
+        this.get('#/login', function () {
             showLoginMenu(selector);
         });
 
-        this.get('#/register', function() {
-            $('#main-container').empty().load('templates/register.html');
+        this.get('#/register', function () {
+            $('#main-container').empty().load('templates/register.html', null, function () {
+                $('#register-button').on('click', function () {
+                    var username = $('#reg-username').val();
+                    var password = $('#reg-password').val();
+                    var name = $('#reg-name').val();
+                    var about = $('#reg-about').val();
+                    var gender = $('input[name=gender-radio]:checked').val();
+                    app.data.users.register(username,password,name,about,gender,1);
+                })
+            });
 
         });
 
-        this.get('#/students', function() {
+        this.get('#/students', function () {
             showStudents(selector);
         });
     });
