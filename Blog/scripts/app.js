d4db01c87debd6f58b3c5f89b7d5ea8f18c70935
Vik-Karaulanov <vkaraulanov@gmail.com>
2016-03-15 15:40:53 +0200

    updeted files(Oakano)

--- Blog/scripts/app.js
+++ Blog/scripts/app.js
@@ -1,1 +1,26 @@
 var app = app || {};
+
+(function() {
+    app.router = Sammy(function () {
+        var selector = '#wrapper';
+
+        this.get('#/', function() {
+            showHomeMenu(selector);
+        });
+
+        this.get('#/login', function() {
+            showLoginMenu(selector);
+        });
+
+        this.get('#/register', function() {
+            $('main').empty().load('templates/register.html');
+
+        });
+
+        this.get('#/students', function() {
+            showStudents(selector);
+        });
+    });
+
+    app.router.run('#/');
+}());
